c8fe9d5b2e63aaa8459eebbb0c93794608a4196e
DongjinS <sdjsdj3@gmail.com>
2021-12-21 05:02:59 +0000

    adder.c form tag \content 추가

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -24,13 +24,26 @@ int main(void) {
       n1 = atoi(first_num);
       n2 = atoi(second_num);
   }
+  /*  
+      content 추가
+      <form align="middle" action="/cgi-bin/adder" method="GET">
+        <p>first number: <input type="text" name="first" /></p>
+        <p>second number: <input type="text" name="second" /></p>
+        <input type="submit" value="Submit" />
+    </form>
+  */
   /* Make the response body */
   sprintf(content, "QUERY_STRING=%s", buf);
-  sprintf(content, "Welcome to add.com: ");
+  sprintf(content, "<div align=center>Welcome to add.com: ");
   sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
-  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
+  sprintf(content, "%s<form align='middle' action='/cgi-bin/adder' method='GET'>\r\n",content);
+  sprintf(content, "%s<p>first number: <input type='text' name='first' /></p>\r\n",content);
+  sprintf(content, "%s<p>second number: <input type='text' name='second' /></p>\r\n",content);
+  sprintf(content, "%s<input type='submit' value='Submit' />\r\n",content);
+  sprintf(content, "%s</form>\r\n", content);
+  sprintf(content, "%s<div align=center>The answer is: %d + %d = %d\r\n<p>",
           content, n1, n2, n1 + n2);
-  sprintf(content, "%sThanks for visiting!\r\n", content);
+  sprintf(content, "%s<div align=center>Thanks for visiting!\r\n", content);
   /* Generate the HTTP response */
   printf("Connection: close\r\n");
   printf("Content-length: %d\r\n", (int)strlen(content));
